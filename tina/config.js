09c2908b04d72d18aa91db06d094ccbd2990f87f
GOLGO11 <873200646@qq.com>
2025-03-30 01:20:32 +0800

    初始化

---
+++ tina/config.js
@@ -0,0 +1,3 @@
+export const apiURL = "https://api.tina.io";
+export const clientId = "6aa4c727-47ab-4366-9095-b7f7ef92282c";  // 替换为你的 Client ID
+export const siteURL = "https://golgo11.github.io/leeao-anthology";  // 替换为你站点的 URL
